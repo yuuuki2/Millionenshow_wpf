795c24572cbd955d3747d85da2551b726d90d975
Sascha Zhang <sascha.zhang@student.htl-rankweil.at>
2024-05-07 11:31:44 +0200

    added a new lib

---
+++ MillionenshowDAL/Class1.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace MillionenshowDAL
+{
+    public class DBAccess
+    {
+
+    }
+}
--- MillionenshowModel/Question.cs
+++ MillionenshowModel/Question.cs
@@ -6,16 +6,21 @@ namespace MillionenshowModel
     public class Question
     {
         public string Text {  get; set; }
-        public  string answerA {  get; set; }
-        public  string answerB {  get; set; }
-        public  string answerC {  get; set; }
-        public  string answerD {  get; set; }
+        public  string AnswerA {  get; set; }
+        public  string AnswerB {  get; set; }
+        public  string AnswerC {  get; set; }
+        public  string AnswerD {  get; set; }
 
         public char CorrectAnwser { get; set; }
 
         public Question(string text, string ansA, string ansB, string ansC, string ansD, char correct) 
         {
-
+            Text = text;
+            AnswerA = ansA;
+            AnswerB = ansB;
+            AnswerC = ansC;
+            AnswerD = ansD;
+            CorrectAnwser = correct;
         }
 
 
